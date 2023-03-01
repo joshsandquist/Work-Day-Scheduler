f2497a78d9834017bca7ac8f5974452eda53c560
Josh Sandquist <joshdsand@gmail.com>
2023-03-01 10:11:56 -0700

    minor bug fixes to make sure time classes match dayJs time

--- script.js
+++ script.js
@@ -1,6 +1,4 @@
-// Wrap all code that interacts with the DOM in a call to jQuery to ensure that
-// the code isn't run until the browser has finished rendering all the elements
-// in the html.
+// This code wil not run until the HTML is rendered using the .ready method
 $(document).ready(function () {
 var saveButtonEl = $('.saveBtn')
 saveButtonEl.on('click', function() {
@@ -13,29 +11,23 @@ var description = $(this).siblings('.description').val().trim();
 //This line will save our data to local storage using hourId as a key and description as a value
 localStorage.setItem(hourId, description)
 })
-  // TODO: Add code to apply the past, present, or future class to each time
-  // block by comparing the id to the current hour. HINTS: How can the id
-  // attribute of each time-block be used to conditionally add or remove the
-  // past, present, and future classes? How can Day.js be used to get the
-  // current hour in 24-hour time?
   // This will retrieve the current hour for comparison in selecting the proper class
 var currentHour = dayjs().hour()
+//The .each method is used to loop through all instances of the .time-block class
 $('.time-block').each(function() {
   //This variable uses the split method to retieve the hour number from hour id
   //The index of [1] retrieves the second word, in this case the hour
-  //parseInt is used to return this string as a number
-  var hour = parseInt($(this).attr('id').split('-')[1])
+  var hour = $(this).attr('id').split('-')[1]
   // conditional logic to add the correct class based on the curent class
   if (hour < currentHour) {
     $(this).addClass('past')
-  } else if ( hour === currentHour) {
+  //used the regular eqauality operator as we are comparing a string number to a primative number
+  } else if ( hour == currentHour) {
     $(this).addClass('present')
   } else if (hour > currentHour) {
     $(this).addClass('future')
   }
-}
-
-)
+})
   // TODO: Add code to get any user input that was saved in localStorage and set
   // the values of the corresponding textarea elements. HINT: How can the id
   // attribute of each time-block be used to do this?
